28522f4f83a2b17abe1fb6bf1baedffb51d1307f
SHUBHA GURUDEV <89259692+Shubhagurudev@users.noreply.github.com>
2023-12-07 21:51:09 +0530

    Add files via upload

---
+++ Phase4Project1Nuni&MoqTesting/Library/Teachers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p4pro1NunitAndMoq
+{
+    public class Teachers
+    {
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public int SubjectId { get; set; }
+
+        public void AcceptData(int tid, string tname, int sid)
+        {
+            TeacherId = tid;
+            TeacherName = tname;
+            SubjectId = sid;
+        }
+    }
+}
---
+++ Phase4Project1Nuni&MoqTesting/TeacherUnitTesting.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using p4pro1NunitAndMoq;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace NunitToDoUnitTesting
+{
+    [TestFixture]
+    public class TeacherUnitTesting
+    {
+        [Test]
+        public void TeacherTest()
+        {
+            Teachers t = new Teachers();
+            t.TeacherId = 1;
+            t.TeacherName = "Sangeetha";
+            t.SubjectId = 1000;
+
+            Teachers t1 = new Teachers();
+            t1.AcceptData(1, "Sangeetha", 1000);
+
+            ClassicAssert.AreEqual(t.TeacherId, t1.TeacherId);
+            ClassicAssert.AreEqual(t.TeacherName, t1.TeacherName);
+            ClassicAssert.AreEqual(t.SubjectId, t1.SubjectId);
+
+
+            Teachers t2 = new Teachers();
+            t2.TeacherId = 2;
+            t2.TeacherName = "Tejaswini";
+            t2.SubjectId = 2000;
+
+            Teachers t3 = new Teachers();
+            t3.AcceptData(2, "Tejaswini", 2000);
+
+            ClassicAssert.AreEqual(t2.TeacherId, t3.TeacherId);
+            ClassicAssert.AreEqual(t2.TeacherName, t3.TeacherName);
+            ClassicAssert.AreEqual(t2.SubjectId, t3.SubjectId);
+        }
+    }
+}
+
